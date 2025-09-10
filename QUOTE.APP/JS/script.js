f0782be06ba4e8d3777a341dee97e71692d66508
Solanki Divya <divusolanki07@gmail.com>
2025-09-10 17:33:05 +0530

    Add files via upload

---
+++ QUOTE.APP/JS/script.js
@@ -0,0 +1,170 @@
+let datas = [{
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/0/02/Nelson_Mandela_1994.jpg",
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+    birthPlace: "Mvezo, South Africa",
+}, {
+    imgurl: "https://m.media-amazon.com/images/M/MV5BNjBlZjMwNGQtOTdlMy00Mzg5LWIzNDQtMzM3YWVhNmQyYTdmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+    birthPlace: "Chicago, Illinois, USA",
+}, {
+    imgurl: "https://imageio.forbes.com/specials-images/imageserve/5b8576db31358e0429c734e3/0x0.jpg?format=jpg&crop=2170,2172,x211,y900,safe&height=416&width=416&fit=bounds",
+    quote: "Your time is limited, so don't waste it living someone else's life.",
+    author: "Steve Jobs",
+    birthPlace: "San Francisco, California, USA",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/CT_74-53_%283x4_cropped%29.jpg/250px-CT_74-53_%283x4_cropped%29.jpg",
+    quote: "If life were predictable it would cease to be life, and be without flavor.",
+    author: "Eleanor Roosevelt",
+    birthPlace: "New York City, New York, USA",
+}, {
+    imgurl: "https://m.media-amazon.com/images/S/amzn-author-media-prod/scu4qti103e8rupc57dl2f2j96.jpg",
+    quote: "If you look at what you have in life, you'll always have more.",
+    author: "Oprah Winfrey",
+    birthPlace: "Kosciusko, Mississippi, USA",
+}, {
+    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjycZ4G5UDcfFhu5RbQB9dppYYQtO_8Z8F2Q&s",
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+    birthPlace: "Liverpool, England",
+}, {
+    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-GyJTZ8RzjIXKgUx9dwuJY9rYidk7zeoQeQ&s",
+    quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+    author: "Mother Teresa",
+    birthPlace: "Skopje, North Macedonia",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/f/fd/FDR-1944-Campaign-Portrait_%283x4_retouched%2C_cropped%29.jpg",
+    quote: "When you reach the end of your rope, tie a knot in it and hang on.",
+    author: "Franklin D. Roosevelt",
+    birthPlace: "Hyde Park, New York, USA",
+}, {
+    imgurl: "https://d3n8a8pro7vhmx.cloudfront.net/ohiocitizenaction/pages/1374/attachments/original/1565115222/mead2.jpg?1565115222",
+    quote: "Always remember that you are absolutely unique. Just like everyone else.",
+    author: "Margaret Mead",
+    birthPlace: "Philadelphia, Pennsylvania, USA",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Robert_Louis_Stevenson_Knox_Series.jpg",
+    quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+    author: "Robert Louis Stevenson",
+    birthPlace: "Edinburgh, Scotland",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/CT_74-53_%283x4_cropped%29.jpg/250px-CT_74-53_%283x4_cropped%29.jpg",
+    quote: "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+    birthPlace: "New York City, New York, USA",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Benjamin_Franklin_1767.jpg",
+    quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    author: "Benjamin Franklin",
+    birthPlace: "Boston, Massachusetts, USA",
+}, {
+    imgurl: "https://ialabs.ie/wp-content/uploads/2023/06/GettyImages-635752305-e1610538598206-copy-48e2.jpg",
+    quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+    author: "Helen Keller",
+    birthPlace: "Tuscumbia, Alabama, USA",
+}, {
+    imgurl: "https://cdn.britannica.com/70/145570-050-6C571884/Aristotle-Stagira-Greece.jpg",
+    quote: "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle",
+    birthPlace: "Stagira, Greece",
+}, {
+    imgurl: "https://www.historyhit.com/app/uploads/2020/07/photo-of-anne-frank-1.jpg",
+    quote: "Whoever is happy will make others happy too.",
+    author: "Anne Frank",
+    birthPlace: "Frankfurt, Germany",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Ralph_Waldo_Emerson_by_Josiah_Johnson_Hawes_1857.jpg",
+    quote: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+    author: "Ralph Waldo Emerson",
+    birthPlace: "Boston, Massachusetts, USA",
+}, {
+    imgurl: "https://hips.hearstapps.com/hmg-prod/images/maya_angelou_photo_by_deborah_feingold_corbis_entertainment_getty_533084708.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*",
+    quote: "You will face many defeats in life, but never let yourself be defeated.",
+    author: "Maya Angelou",
+    birthPlace: "St. Louis, Missouri, USA",
+}, {
+    imgurl: "https://i0.wp.com/historycolored.com/wp-content/uploads/2021/12/Abe-LincolnColor.jpg?fit=1008%2C1169&ssl=1",
+    quote: "In the end, it's not the years in your life that count. It's the life in your years.",
+    author: "Abraham Lincoln",
+    birthPlace: "Hodgenville, Kentucky, USA",
+}, {
+    imgurl: "https://m.media-amazon.com/images/M/MV5BOTAyMDZkNTQtYWRiNS00ZjZmLWFmYjktYTYzN2ZhMzE5NzU4XkEyXkFqcGc@._V1_.jpg",
+    quote: "Never let the fear of striking out keep you from playing the game.",
+    author: "Babe Ruth",
+    birthPlace: "Baltimore, Maryland, USA",
+}, {
+    imgurl: "https://ialabs.ie/wp-content/uploads/2023/06/GettyImages-635752305-e1610538598206-copy-48e2.jpg",
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+    birthPlace: "Tuscumbia, Alabama, USA",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Thomas_Edison2.jpg",
+    quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+    author: "Thomas A. Edison",
+    birthPlace: "Milan, Ohio, USA",
+}, {
+    imgurl: "https://images1.penguinrandomhouse.com/author/2065083",
+    quote: "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+    author: "Brian Tracy",
+    birthPlace: "Charlottetown, Canada",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Theodore_%22Teddy%22_Roosevelt.png",
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+    birthPlace: "New York City, New York, USA",
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/9/9c/William_James_b1842c.jpg",
+    quote: "Act as if what you do makes a difference. It does.",
+    author: "William James",
+    birthPlace: "New York City, New York, USA"
+}, {
+    imgurl: "https://www.meisterdrucke.us/kunstwerke/1260px/Unbekannt_-_Winston_Churchill_making_his_famous_V_for_Victory_sign_1942_-_%28MeisterDrucke-755720%29.jpg",
+    quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+    birthPlace: "Woodstock, England"
+}, {
+    imgurl: "https://cdn.britannica.com/24/82724-050-A1F9D0B9/CS-Lewis.jpg",
+    quote: "You are never too old to set another goal or to dream a new dream.",
+    author: "C.S. Lewis",
+    birthPlace: "Belfast, Ireland"
+}, {
+    imgurl: "https://m.media-amazon.com/images/S/amzn-author-media-prod/qculg5dpfef7827m0jskcun5b1.jpg",
+    quote: "At any given moment you have the power to say: this is not how the story is going to end.",
+    author: "Christine Mason Miller",
+    birthPlace: "Unknown",
+}, {
+    imgurl: "https://static01.nyt.com/images/2012/11/30/business/Ziglar2/Ziglar2-superJumbo.jpg",
+    quote: "You don't have to be great to start, but you have to start to be great.",
+    author: "Zig Ziglar",
+    birthPlace: "Coffee County, Alabama, USA"
+
+}, {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Thomas_Edison2.jpg",
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    author: "Thomas A. Edison",
+    birthPlace: "Milan, Ohio, USA",
+}, {
+
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/b/b5/Samlevenson.jpg",
+    birthPlace: "New York City, New York, USA",
+}
+];
+
+str = '';
+datas.forEach((datas) => {
+
+    str += `<div class="box-12">
+                <div class="box-5">
+                    <div class="card">
+                        <div class="aImage"> <img src="${datas.imgurl}" alt="pics"> </div>
+                        <div class="aQuote">${datas.quote}</div>
+                        <div class="aAuthor"><a href="#" style="text-decoration: underline; color: black";>${datas.author}</a></div>
+                        <div class="aPlace">${datas.birthPlace}</div>  
+                    </div>
+                </div>`;
+})
+
+document.getElementById("datas").innerHTML = str;
